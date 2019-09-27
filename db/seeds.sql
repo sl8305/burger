00c146f4f7476ada1334ce87567242a593976d99
Blue Bean <sl8305@hotmail.com>
2019-09-26 19:41:45 -0700

    connection.js and orm.js is complete

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,5 @@
 -- Inserting three starter values for the database of burgers
 
-INSERT INTO burgers (name) VALUES ('Bacon Cheeseburger');
-INSERT INTO burgers (name) VALUES ('Mushroom Burger');
-INSERT INTO burgers (name) VALUES ('BLT');
+INSERT INTO burgers (burger_name) VALUES ('Bacon Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Mushroom Burger');
+INSERT INTO burgers (burger_name) VALUES ('BLT');
